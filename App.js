d7ff22b318a5ce1e3c66bf475e0e352ecb322fdf
Yodit Fentaw <yoditfentaw90@gmail.com>
2022-03-31 15:28:38 -0700

    commit #2

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { Icon } from 'react-native-vector-icons/icon';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button,
+  SafeAreaView, 
+  Image 
+} from 'react-native';
 
+
+const HomeStack = createNativeStackNavigator();
+const DetailsStack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeStackScreen  = ({navigation}) => (
+       <HomeStack.Navigator screenOptions={{
+         headerStyle: {
+            backgroundColor: '#009387'
+          },
+          headerTintColor: '#fff',
+          headerTintStyle: {
+            fontWeight: 'bold'
+          }
+      }}>
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{
+          title: "Overview",
+          // headerLeft: () => (
+          //   <Icon.Button 
+          //     name="ios-menu" 
+          //     size={25} 
+          //     backgroundColor="#009387"
+          //     options={()=> {
+          //       navigation.openDrawer()}
+          //       }>
+          //   </Icon.Button>
+          // )
+        }} />
+      </HomeStack.Navigator> 
+);
+
+const DetailsStackScreen  = ({navigation}) => (
+       <DetailsStack.Navigator screenOptions={{
+         headerStyle: {
+            backgroundColor: '#009387'
+          },
+          headerTintColor: '#fff',
+          headerTintStyle: {
+            fontWeight: 'bold'
+          }
+      }}>
+        <DetailsStack.Screen name="Details" component={DetailsScreen} options={{
+        }} />
+      </DetailsStack.Navigator> 
+);
 
 export default function App() {
   return (
-    <View style={styles.root}>
-      <Text>Hello, this is expo project</Text>
-    </View>
+    <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={HomeStackScreen} />
+            <Drawer.Screen name="Details" component={DetailsStackScreen} />
+          </Drawer.Navigator> 
+    </NavigationContainer>
   );
 }
 
