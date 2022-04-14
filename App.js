6ea409c020f1d05d0490f51abe8d02f2b8ad4d08
Yodit Fentaw <yoditfentaw90@gmail.com>
2022-04-14 12:08:28 -0700

    bottom bar

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { Icon } from 'react-native-vector-icons/icon';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
+import { DrawerContent } from './src/screens/DrawerContent';
+
+
 import { 
   StyleSheet, 
   Text, 
@@ -13,59 +13,17 @@ import {
   SafeAreaView, 
   Image 
 } from 'react-native';
+import MainTabScreen from './src/screens/MainTabScreen';
 
-
-const HomeStack = createNativeStackNavigator();
-const DetailsStack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-const HomeStackScreen  = ({navigation}) => (
-       <HomeStack.Navigator screenOptions={{
-         headerStyle: {
-            backgroundColor: '#009387'
-          },
-          headerTintColor: '#fff',
-          headerTintStyle: {
-            fontWeight: 'bold'
-          }
-      }}>
-        <HomeStack.Screen name="Home" component={HomeScreen} options={{
-          title: "Overview",
-          // headerLeft: () => (
-          //   <Icon.Button 
-          //     name="ios-menu" 
-          //     size={25} 
-          //     backgroundColor="#009387"
-          //     options={()=> {
-          //       navigation.openDrawer()}
-          //       }>
-          //   </Icon.Button>
-          // )
-        }} />
-      </HomeStack.Navigator> 
-);
-
-const DetailsStackScreen  = ({navigation}) => (
-       <DetailsStack.Navigator screenOptions={{
-         headerStyle: {
-            backgroundColor: '#009387'
-          },
-          headerTintColor: '#fff',
-          headerTintStyle: {
-            fontWeight: 'bold'
-          }
-      }}>
-        <DetailsStack.Screen name="Details" component={DetailsScreen} options={{
-        }} />
-      </DetailsStack.Navigator> 
-);
 
 export default function App() {
   return (
     <NavigationContainer>
-          <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={HomeStackScreen} />
-            <Drawer.Screen name="Details" component={DetailsStackScreen} />
+          <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={MainTabScreen} />
+            {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
           </Drawer.Navigator> 
     </NavigationContainer>
   );
---
+++ src/screens/DrawerContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleShit } from 'react-native-web';
+import { 
+    DrawerContentScrollView,
+    DrawerItem
+ } from '@react-navigation/drawer';
+
+ export function DrawerContent(props) {
+
+     <View>
+         <Text>this is a test</Text>
+     </View>
+ }
